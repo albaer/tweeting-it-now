807cd6945b57b05e553fe83682152be467398a00
Anne Baer <albaer@gmail.com>
2014-05-28 13:52:47 -0500

    adding ajax for the tweets

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+	$("#tweet-it").on("submit", function() {
+		$(".submit").prop('disabled', true);
+		$("#done").remove();
+		$("#tweet-it").after("<p id=\"processing\">Your tweet is being processed right now!</p>")
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+		var url = $("#tweet-it").attr("action");
+		var data = $("#tweet-it").serialize();
+
+		$.post(url, data, function(response){
+			$("#processing").remove();
+			$("#tweet-it").after("<p id=\"done\">Your tweet has been tweeted!</p>")
+		}); //closes post request
+	}); // closes event handling
+}); //closes doc ready
